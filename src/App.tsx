a62f4aa8914f7ad2509d28e12fecdabbcbaf58d2
marbello1973 <david.marbello@outlook.com>
2025-02-10 22:12:10 -0500

    fondo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import styles from './App.module.css'
 import { Circulo } from './component/circulo/Circulo'
+import { Lineas2 } from './component/lieneas-ondulada2/Lineas2'
+import { Lineas1 } from './component/lineas-ondulada1/Lineas1'
 import { Lineas } from './component/lineas/Lineas'
 
-function App() {
- 
+function App() { 
 
   return (    
       <div className={styles.container}>  
-        <div><Lineas/>
-          <div className={styles.circulo}><Circulo/></div>        
+        <div>
+          <Lineas/>
+          <div className={styles.circulo}><Circulo/></div>  
+          <div className={styles.lineas1}><Lineas1/></div>
+          <div className={styles.lineas2}><Lineas2/></div>      
         </div> 
       </div>
       
