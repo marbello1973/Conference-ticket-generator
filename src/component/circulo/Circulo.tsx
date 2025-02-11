bf95f25d94d204bdf2a44d9fe231e0b0ef362b01
marbello1973 <david.marbello@outlook.com>
2025-02-10 19:49:26 -0500

    fondo

---
+++ src/component/circulo/Circulo.tsx
@@ -0,0 +1,14 @@
+//import styles from './Circulo.module.css'
+
+export const Circulo = () => {
+  return (
+    <div>
+        <svg width="217" height="217" fill="none" viewBox="0 0 217 217">
+            <g stroke="#fff" opacity=".1">
+                <circle cx="108.5" cy="108.5" r="54"/>
+                <circle cx="108.5" cy="108.5" r="108"/>
+            </g>
+        </svg>
+    </div>
+  )
+}
